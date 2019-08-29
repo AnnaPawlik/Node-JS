8dc360b96e68ad15915cab2c766d40362e8f6876
Anna Pawlik <pawlikanna24@gmail.com>
2019-08-29 10:13:26 +0200

    Add new folders

--- app/program.js
+++ app/program.js
@@ -1,19 +1,26 @@
-process.stdin.setEncoding('utf-8');
+var OSinfo = require('../modules/OSInfo');
 
-process.stdin.on('readable', function(){
-    var input = process.stdin.read();
+process.stdin.setEncoding('utf-8'); 
+
+process.stdin.on('readable', function(){ 
+    var input = process.stdin.read(); 
     if(input !== null) {
         var instruction = input.toString().trim();
         switch (instruction) {
-            case 'exit':
+            case '/exit':
                 process.stdout.write('Quitting app!\n');
                 process.exit();
                 break;
-            case 'version':
+            case '/version':
                 console.log(process.env);
+                break;
+            case '/getOSinfo':
+                getOSinfo();
                 break;
             default:
                 process.stderr.write('Wrong instruction!');
         }
     }
 });
+
+
